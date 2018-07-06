f9824306db6f25ed2c768e5b5ddcbe8e8bd50afd
Nikita Bhambhani <nikita_bhambhani@intuit.com>
2018-07-06 12:04:50 -0700

    Update sample to use new OAuth2Client

--- MvcCodeFlowClientManual/Controllers/AppController.cs
+++ MvcCodeFlowClientManual/Controllers/AppController.cs
@@ -12,6 +12,7 @@ using Intuit.Ipp.QueryFilter;
 using Intuit.Ipp.Security;
 using System.Linq;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace MvcCodeFlowClientManual.Controllers
 {
@@ -20,54 +21,19 @@ namespace MvcCodeFlowClientManual.Controllers
         public static string clientid = ConfigurationManager.AppSettings["clientid"];
         public static string clientsecret = ConfigurationManager.AppSettings["clientsecret"];
         public static string redirectUrl = ConfigurationManager.AppSettings["redirectUrl"];
+        public static string environment = ConfigurationManager.AppSettings["appEnvironment"];
 
-        public static string authorizeUrl = "";
-        public static string tokenEndpoint = "";
-        public static string code = "";
-
-        public static string access_token = "";
-        public static string refresh_token = "";
-        DiscoveryClient discoveryClient;
-        DiscoveryResponse doc;
-        public static string scope;
+        public static OAuth2Client auth2Client = new OAuth2Client(clientid, clientsecret, redirectUrl, environment);
 
         /// <summary>
         /// Use the Index page of App controller to get all endpoints from discovery url
         /// </summary>
-        public async Task<ActionResult> Index()
+        public ActionResult Index()
         {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-
             Session.Clear();
             Session.Abandon();
             Request.GetOwinContext().Authentication.SignOut("Cookies");
-
-            //Intialize DiscoverPolicy on page load
-            DiscoveryPolicy dpolicy = new DiscoveryPolicy();
-            dpolicy.RequireHttps = true;
-            dpolicy.ValidateIssuerName = true;
-
-            //Read discovery url from config
-            string discoveryUrl = ConfigurationManager.AppSettings["DiscoveryUrl"];
-
-            if (discoveryUrl != null && clientid != null && clientsecret != null)
-            {
-                discoveryClient = new DiscoveryClient(discoveryUrl);
-            }
-            else
-            {
-                Exception ex= new Exception("Discovery Url missing!");
-                throw ex;
-            }
-            doc = await discoveryClient.GetAsync();
-
-            //If discovery url is working then get all Authorization endpoints
-            if (doc.StatusCode == HttpStatusCode.OK)
-            {
-                authorizeUrl = doc.AuthorizeEndpoint;
-                tokenEndpoint = doc.TokenEndpoint;
-            }
-
             return View();
         }
 
@@ -79,22 +45,10 @@ namespace MvcCodeFlowClientManual.Controllers
             switch (submitButton)
             {
                 case "Connect to QuickBooks":
-                    scope = OidcScopes.Accounting.GetStringValue();
-
-                    var request = new AuthorizeRequest(authorizeUrl);
-
-                    var state = Guid.NewGuid().ToString("N");
-                    SetTempState(state);
-
-                    authorizeUrl = request.CreateAuthorizeUrl(
-                       clientId: clientid,
-                       responseType: OidcConstants.AuthorizeResponse.Code,
-                       scope: scope,
-                       redirectUri: redirectUrl,
-                       state: state);
-
+                    List<OidcScopes> scopes = new List<OidcScopes>();
+                    scopes.Add(OidcScopes.Accounting);
+                    string authorizeUrl = auth2Client.GetAuthorizationURL(scopes);
                     return Redirect(authorizeUrl);
-
                 default:
                     return (View());
             }
@@ -112,13 +66,13 @@ namespace MvcCodeFlowClientManual.Controllers
                 {
                     var principal = User as ClaimsPrincipal;
                     OAuth2RequestValidator oauthValidator = new OAuth2RequestValidator(principal.FindFirst("access_token").Value);
-                    
+
                     // Create a ServiceContext with Auth tokens and realmId
                     ServiceContext serviceContext = new ServiceContext(realmId, IntuitServicesType.QBO, oauthValidator);
                     serviceContext.IppConfiguration.MinorVersion.Qbo = "23";
 
                     // Create a QuickBooks QueryService using ServiceContext
-                    QueryService<CompanyInfo> querySvc = new QueryService<CompanyInfo>(serviceContext); 
+                    QueryService<CompanyInfo> querySvc = new QueryService<CompanyInfo>(serviceContext);
                     CompanyInfo companyInfo = querySvc.ExecuteIdsQuery("SELECT * FROM CompanyInfo").FirstOrDefault();
 
                     string output = "Company Name: " + companyInfo.CompanyName + " Company Address: " + companyInfo.CompanyAddr.Line1 + ", " + companyInfo.CompanyAddr.City + ", " + companyInfo.CompanyAddr.Country + " " + companyInfo.CompanyAddr.PostalCode;
@@ -147,19 +101,6 @@ namespace MvcCodeFlowClientManual.Controllers
         public ActionResult Tokens()
         {
             return View("Tokens");
-        }
-
-        /// <summary>
-        /// Create a temp state to add new claim info for logged in user
-        /// </summary>
-        private void SetTempState(string state)
-        {
-            //Assign temp state for the logged in user's claims
-            var tempId = new ClaimsIdentity("TempState");
-            tempId.AddClaim(new Claim("state", state));
-           
-            //Sign in with temp state
-            Request.GetOwinContext().Authentication.SignIn(tempId);
         }
     }
 }
--- MvcCodeFlowClientManual/Controllers/CallbackController.cs
+++ MvcCodeFlowClientManual/Controllers/CallbackController.cs
@@ -9,17 +9,15 @@ using System.Web.Mvc;
 namespace MvcCodeFlowClientManual.Controllers
 {
     public class CallbackController : Controller
-	{
+    {
         /// <summary>
         /// Code and realmid/company id recieved on Index page after redirect is complete from Authorization url
         /// </summary>
         public async Task<ActionResult> Index()
-		{
-            //Sync the state info and update if if it is not the same
+        {
+            //Sync the state info and update if it is not the same
             var state = Request.QueryString["state"];
-            var tempState = await GetTempStateAsync();
-
-            if (state.Equals(tempState.Item1, StringComparison.Ordinal))
+            if (state.Equals(AppController.auth2Client.CSRFToken, StringComparison.Ordinal))
             {
                 ViewBag.State = state + " (valid)";
             }
@@ -47,10 +45,8 @@ namespace MvcCodeFlowClientManual.Controllers
                 Session["realmId"] = realmId;
             }
 
-            var tokenClient = new TokenClient(AppController.tokenEndpoint, AppController.clientid, AppController.clientsecret);
-
             Request.GetOwinContext().Authentication.SignOut("TempState");
-            TokenResponse tokenResponse = await tokenClient.RequestTokenFromCodeAsync(code, AppController.redirectUrl);
+            var tokenResponse = await AppController.auth2Client.GetBearerTokenAsync(code);
 
             var claims = new List<Claim>();
 
@@ -74,15 +70,5 @@ namespace MvcCodeFlowClientManual.Controllers
             var id = new ClaimsIdentity(claims, "Cookies");
             Request.GetOwinContext().Authentication.SignIn(id);
         }
-
-        /// <summary>                    
-        /// Get state token
-        /// </summary>
-        private async Task<Tuple<string>> GetTempStateAsync()
-        {
-            var data = await Request.GetOwinContext().Authentication.AuthenticateAsync("TempState");
-            var state = data.Identity.FindFirst("state").Value;
-            return Tuple.Create(state);
-        }
-	}
+    }
 }
